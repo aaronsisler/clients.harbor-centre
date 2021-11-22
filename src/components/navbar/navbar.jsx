761bcf6de0d15781206cb06b1e51c1ecec777be7
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-11-22 08:04:00 -0500

    Holding new navbar pieces

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,49 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import cn from "classnames";
-
-import Logo from "../logo";
-import Hyperlink from "../../atoms/hyperlink";
+import Logo from "../../components/logo";
+import AccordionDesktop from "../../atoms/accordion-desktop";
+import AccordionMobile from "../../atoms/accordion-mobile";
 import Hamburger from "../../atoms/hamburger";
+import navigationLinks from "../../content/navigation-links";
 
-import navbarStyles from "./navbar.module.scss";
+import styles from "./navbar.module.scss";
 
-const Navbar = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const menuToggledClass = isMenuOpen ? navbarStyles.navbar__open : undefined;
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
 
-  useEffect(() => {
-    if (isMenuOpen) {
-      document.body.classList.add("body__noScroll");
-    } else {
-      document.body.classList.remove("body__noScroll");
-    }
-  });
+  handleMenuToggle = () =>
+    this.setState((prevState) => ({
+      isMenuOpen: !prevState.isMenuOpen,
+    }));
 
-  return (
-    <nav className={navbarStyles.navbar}>
-      <Logo className={navbarStyles.navbar__logo} />
-      <div className={navbarStyles.navbar__toggle}>
-        <Hamburger
-          isToggled={isMenuOpen}
-          onClick={() => setMenuOpen(!isMenuOpen)}
-        />
-      </div>
-      <div className={cn(navbarStyles.navbar__links, menuToggledClass)}>
-        <Hyperlink
-          className={navbarStyles.navbar__link}
-          href="/"
-          onClick={() => setMenuOpen(false)}
-          title="Home"
-        />
-        <Hyperlink
-          className={navbarStyles.navbar__link}
-          href="/contact"
-          onClick={() => setMenuOpen(false)}
-          title="Contact"
-        />
-      </div>
-    </nav>
-  );
-};
+  render() {
+    const { isMenuOpen } = this.state;
+    const menuToggledClass = isMenuOpen ? styles.navbar__linksOpen : undefined;
+
+    return (
+      <nav className={styles.navbar}>
+        <div className={styles.navbar__toggle}>
+          <Hamburger isToggled={isMenuOpen} onClick={this.handleMenuToggle} />
+        </div>
+        <div className={styles.navbar__logo}>
+          <Logo />
+        </div>
+        <div className={cn(styles.navbar__linksMobile, menuToggledClass)}>
+          <div className={cn(styles.navbar__linksContent)}>
+            {navigationLinks.map((navLink) => (
+              <AccordionMobile
+                key={navLink.key}
+                onContentClick={this.handleMenuToggle}
+                {...navLink}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={cn(styles.navbar__linksDesktop, menuToggledClass)}>
+          {navigationLinks.map((navLink) => (
+            <AccordionDesktop key={navLink.key} {...navLink} />
+          ))}
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
---
+++ src/content/navigation-links.js
@@ -0,0 +1,23 @@
+const navigationLinks = [
+  {
+    key: "experience",
+    title: "Experience",
+    links: [
+      { title: "Our Tenants", href: "/tenants" },
+      { title: "Floorplans", href: "/Floorplans" },
+      { title: "Rooftop", href: "/rooftop" },
+    ],
+  },
+  {
+    key: "location",
+    title: "Location",
+    links: [{ title: "Main Street", href: "/main-street" }],
+  },
+  {
+    key: "about",
+    title: "About Us",
+    links: [{ title: "Contact Us", href: "/contact" }],
+  },
+];
+
+export default navigationLinks;
