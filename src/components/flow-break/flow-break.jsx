22de3286345e8151aef67bf85013bb39bae49afb
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-11-24 18:26:31 -0500

    Updating for the stylelint changes/errors and tweaking imports

--- src/components/flow-break/flow-break.jsx
+++ src/components/flow-break/flow-break.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import styles from "./flow-break.module.scss";
 
-const FlowBreak = () => <div>FlowBreak</div>;
+const FlowBreak = () => <div className={styles.flowBreak}></div>;
 
 export default FlowBreak;
