c0e28f3106564279caf959d40c5e2cb112164435
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-12-28 19:11:14 -0500

    Adding new format for tenant

--- src/components/tenant/tenant.jsx
+++ src/components/tenant/tenant.jsx
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import ExternalLink from "../../atoms/external-link";
+
 import styles from "./tenant.module.scss";
 
-const Tenant = ({ bio, link, location, name }) => (
-  <div className={styles.tenant}>
-    <div className={styles.tenant__name}>{name}</div>
-    {link && (
-      <ExternalLink
-        className={styles.tenant__link}
-        href={link}
-        title="Link to tenant's site"
-      />
-    )}
-    <div className={styles.tenant__bio}>{bio}</div>
-    <div className={styles.tenant__location}>Location:&nbsp;{location}</div>
-  </div>
-);
+const Tenant = ({ bio, link, location, name }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className={styles.tenant}>
+      <div
+        className={styles.tenant__card}
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {name}
+      </div>
+      {isExpanded && (
+        <div className={styles.tenant__expanded}>
+          {link && (
+            <ExternalLink
+              className={styles.tenant__link}
+              href={link}
+              title="Link to tenant's site"
+            />
+          )}
+          <div className={styles.tenant__bio}>{bio}</div>
+          <div className={styles.tenant__location}>
+            Location:&nbsp;{location}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 Tenant.propTypes = {
-  name: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  link: PropTypes.string,
   bio: PropTypes.string,
+  link: PropTypes.string,
+  location: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Tenant;
