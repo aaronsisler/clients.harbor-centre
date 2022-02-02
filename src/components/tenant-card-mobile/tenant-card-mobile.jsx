471171c4bb79b8fbb3b5a7d447c9bb7699206f8f
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-02-01 20:26:51 -0500

    Tweaks for mobile tenant card and adding in links and cleaning up bio section

--- src/components/tenant-card-mobile/tenant-card-mobile.jsx
+++ src/components/tenant-card-mobile/tenant-card-mobile.jsx
@@ -5,6 +5,14 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Collapse from "@mui/material/Collapse";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import DevicesIcon from "@mui/icons-material/Devices";
+
+import ExternalIconLink from "../../atoms/external-icon-link";
+
+// import DesktopWindowsIcon from '@mui/icons-material/DesktopWindows';
 
 import styles from "./tenant-card-mobile.module.scss";
 
@@ -23,10 +31,40 @@ const TenantCardMobile = ({ bio, images = {}, links, location, name }) => {
           image={images.cardProfile.src}
           alt={images.cardProfile.alt}
         />
-        <div className={styles.cardMain__title}>{name}</div>
+        <div className={styles.cardMain__titleBlock}>
+          <div className={styles.titleBlock__title}>{name}</div>
+          <div className={styles.titleBlock__expand}>
+            <ExpandMoreIcon fontSize="inherit" />
+          </div>
+        </div>
       </CardContent>
       <Collapse in={isExpanded} timeout="auto" unmountOnExit>
-        <CardContent>{bio}</CardContent>
+        <CardContent className={styles.tenantCardMobile__cardContent}>
+          <div className={styles.cardContent__links}>
+            {links?.website && (
+              <ExternalIconLink href={links.website}>
+                <DevicesIcon />
+              </ExternalIconLink>
+            )}
+            {links?.facebook && (
+              <ExternalIconLink href={links.facebook}>
+                <FacebookIcon />
+              </ExternalIconLink>
+            )}
+            {links?.instagram && (
+              <ExternalIconLink href={links.instagram}>
+                <InstagramIcon />
+              </ExternalIconLink>
+            )}
+          </div>
+          <div className={styles.cardContent__bio}>
+            {bio.map((bioLineItem, index) => (
+              <p key={index} className={styles.bio__lineItem}>
+                {bioLineItem}
+              </p>
+            ))}
+          </div>
+        </CardContent>
       </Collapse>
     </Card>
   );
