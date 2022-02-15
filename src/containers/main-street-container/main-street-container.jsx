ce70dfbffc0a5defac37380742751647c7f4ecf5
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-02-15 10:05:44 -0500

    Updating given removal of unused pages and stopping GHA until we determine best repeatable practice

--- src/containers/main-street-container/main-street-container.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./main-street-container.module.scss";
-
-const MainStreetContainer = () => (
-  <div className={styles.mainStreetContainer}>Main Street Container</div>
-);
-
-export default MainStreetContainer;
