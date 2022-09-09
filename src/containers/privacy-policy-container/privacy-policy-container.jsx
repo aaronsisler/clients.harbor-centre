0b0bda5c6fde1f96a08ea5aa486302e5691052e4
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-09-09 13:11:52 -0400

    Tweaks for font size across the app

--- src/containers/privacy-policy-container/privacy-policy-container.jsx
+++ src/containers/privacy-policy-container/privacy-policy-container.jsx
@@ -5,26 +5,26 @@ import styles from "./privacy-policy-container.module.scss";
 
 const PrivacyPolicyContainer = () => (
   <div className={styles.privacyPolicyContainer}>
-    <div className={styles.privacyPolicyContainer__title}>
+    <h1 className={styles.privacyPolicyContainer__title}>
       Privacy Policy for {privacyPolicy.companyWebsite}
-    </div>
-    {privacyPolicy.policies.map((policyLine, index) => {
-      if (policyLine.isBold) {
+    </h1>
+    <section className={styles.privacyPolicyContainer__policies}>
+      {privacyPolicy.policies.map((policyLine, index) => {
+        if (policyLine.isBold) {
+          return (
+            <React.Fragment key={index}>
+              <br />
+              <div className={styles.policy__title}>{policyLine.text}</div>
+            </React.Fragment>
+          );
+        }
         return (
-          <div
-            className={styles.privacyPolicyContainer__policyHeading}
-            key={index}
-          >
+          <div className={styles.policy__content} key={index}>
             {policyLine.text}
           </div>
         );
-      }
-      return (
-        <div className={styles.privacyPolicyContainer__policyText} key={index}>
-          {policyLine.text}
-        </div>
-      );
-    })}
+      })}
+    </section>
   </div>
 );
 
