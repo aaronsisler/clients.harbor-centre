1b45b67ed07c2a38e0a4dfab95941ee9458eb729
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-09-15 07:42:42 -0400

    Adding in background shape comp and tweaking the contact page for new buttons

--- src/containers/contact-container/contact-container.jsx
+++ src/containers/contact-container/contact-container.jsx
@@ -1,24 +1,16 @@
 import React from "react";
+import { BackgroundShape } from "../../components/background-shape";
 
 import styles from "./contact-container.module.scss";
 
 const ContactContainer = () => (
   <div className={styles.contactContainer}>
     <h1 className={styles.contactContainer__title}>What works best for you?</h1>
-    <div className={styles.contactContainer__shape} />
-    <section className={styles.contactContainer__methods}>
-      <div className={styles.contactMethod}>
-        <h2>Visit Us</h2>
-        <div>Map</div>
-      </div>
-      <div className={styles.contactMethod}>
-        <h2>Call Us</h2>
-        <div>Call</div>
-      </div>
-      <div className={styles.contactMethod}>
-        <h2>Write Us</h2>
-        <div>Message</div>
-      </div>
+    <BackgroundShape />
+    <section className={styles.contactContainer__contactMethods}>
+      <button className={styles.contactMethodButton}>Visit Us</button>
+      <button className={styles.contactMethodButton}>Call Us</button>
+      <button className={styles.contactMethodButton}>Write Us</button>
     </section>
   </div>
 );
